7e3dda4d4f26d6d9e735ca07574cdfafb44f2a32
LeanaRain <137782467+LeanaRain@users.noreply.github.com>
2023-10-21 03:50:23 +0500

    feat: complete task

--- src/index.js
+++ src/index.js
@@ -2,5 +2,18 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  if (!matrix || matrix.length === 0) { //если матрица не существует или ее длина = 0
+  return []; //возвращаем пустой массив
+  }
+
+let towels = []; //cозлаем пустой массив для хранения полотенец
+
+for (let i = 0; i < matrix.length; i++) { //цикл перебирает строки матрицы по индексу
+  if (i % 2 === 0) { //проверяется четное ли значение индекса
+    towels = towels.concat(matrix[i]); //если чет - строки добавляются без измненений
+  } else {
+    towels = towels.concat(matrix[i].reverse()); //иначе переворачиваются в обратном порядке
+  }
+}
+return towels; //возврат отсортированного массива
 }
